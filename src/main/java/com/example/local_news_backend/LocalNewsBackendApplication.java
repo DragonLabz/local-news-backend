8e32cffe69d259c7e4bb7a9e5d2ff9e2bf16ebe0
Malinga <malinga_samarakoon@outlook.com>
2025-06-14 04:23:16 +0530

    Initialize the project

---
+++ src/main/java/com/example/local_news_backend/LocalNewsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.local_news_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalNewsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalNewsBackendApplication.class, args);
+	}
+
+}
